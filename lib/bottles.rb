4520535fbe055864c63d19769193a73a727e50a9
Chris Blackburn <cblackburn@ajla.net>
2020-04-23 12:43:31 -0600

    First refactor

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,22 +2,15 @@ require 'pry'
 require_relative 'wall'
 
 class Bottles
-  VERSE_A = "%d %s of beer on the wall, %d %s of beer.\n"
-  VERSE_B = "Take one down and pass it around, %d %s of beer on the wall.\n"
-
   def verse(count)
     @wall = Wall.new(count, 'bottle of beer', 'bottles of beer')
-    output = @wall.verse_a
-    output << @wall.verse_b
-    output
+    @wall.verse
   end
 
   def verses(start_count, end_count)
-    output = []
-    (start_count).downto(end_count).each do |count|
-      output << verse(count)
-    end
-    output.join("\n")
+    (start_count).downto(end_count).map do |count|
+      verse(count)
+    end.join("\n")
   end
 
   def song
--- lib/wall.rb
+++ lib/wall.rb
@@ -6,6 +6,38 @@ class Wall
     @plural_phrase = plural_phrase
   end
 
+  def verse
+    verse_a + verse_b
+  end
+
+  private
+
+  def subject
+    @count == 0 ? 'it' : 'one'
+  end
+
+  def remaining
+    @count == 0 ? 'no more' : @count
+  end
+
+  def verse_a
+    if @count == 0
+      format("No more %s on the wall, no more %s.\n", items, items)
+    else # count > 0
+      format("%d %s on the wall, %d %s.\n", @count, items, @count, items)
+    end
+  end
+
+  def verse_b
+    if @count == 0
+      reset_item_count
+      format("Go to the store and buy some more, %d %s on the wall.\n", @count, items)
+    else # count > 0
+      decrement_item_count
+      format("Take %s down and pass it around, %s %s on the wall.\n", subject, remaining, items)
+    end
+  end
+
   def decrement_item_count
     @count -= 1
   end
@@ -19,29 +51,6 @@ class Wall
       @singular_phrase
     else
       @plural_phrase
-    end
-  end
-
-  def verse_a
-    if @count == 0
-      format("No more %s on the wall, no more %s.\n", items, items)
-    elsif @count == 1
-      format("%d %s on the wall, %d %s.\n", @count, items, @count, items)
-    else # count > 1
-      format("%d %s on the wall, %d %s.\n", @count, items, @count, items)
-    end
-  end
-
-  def verse_b
-    if @count == 0
-      reset_item_count
-      format("Go to the store and buy some more, %d %s on the wall.\n", @count, items)
-    elsif @count == 1
-      decrement_item_count
-      format("Take it down and pass it around, no more %s on the wall.\n", items)
-    else # count > 1
-      decrement_item_count
-      format("Take one down and pass it around, %d %s on the wall.\n", @count, items)
     end
   end
 end
